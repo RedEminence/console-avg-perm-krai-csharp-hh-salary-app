678a19fc05c415c07ef80a2a3ae3b18fd99cbf35
Евгений Голубаев <evgeny.g@digital-spectr.ru>
2021-11-06 16:37:31 +0500

    Removed hardcoded per_page=100 value, added pagination-based requests instead

--- ConsoleHHApp/Program.cs
+++ ConsoleHHApp/Program.cs
@@ -9,23 +9,47 @@ namespace ConsoleAvgPermKraiCSharpHHSalaryApp
     {
         static void Main(string[] args)
         {
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.Add("User-Agent", "CSharp-App");
-            client.DefaultRequestHeaders.Add("Accept", "application/json");
-
-            var baseUrl = "https://api.hh.ru";
-            var vacanciesEndpointWithParams = "/vacancies?text=c%23&area=72&only_with_salary=true&per_page=100";
-            var fullUrl = $"{baseUrl}{vacanciesEndpointWithParams}";
-
-            var response = client.GetAsync(fullUrl).Result;
-            var responseBody = response.Content.ReadAsStringAsync().Result;
-
-            var json = JObject.Parse(responseBody);
-            var vacancies = (JArray)json["items"];
+            JArray vacancies = GetHHVacancies();
 
             var averageCSharpSalary = vacancies.Select(v => GetSalaryForVacancy(v["salary"])).Average();
             
             Console.WriteLine($"Средняя зарплата C#-разработчика в Пермском крае составляет {averageCSharpSalary} рублей");
+        }
+
+        private static JArray GetHHVacancies()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", "CSharp-App");
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            int currentPageNum = 0;
+            string initialUrl = $"https://api.hh.ru/vacancies?text=c%23&area=72&only_with_salary=true&per_page=10&page={currentPageNum}";
+
+            var initialResponse = client.GetAsync(initialUrl).Result;
+            var initialResponseBody = initialResponse.Content.ReadAsStringAsync().Result;
+
+            JObject json = JObject.Parse(initialResponseBody);
+            JArray vacancies = (JArray)json["items"];
+            int pageCount = (int)json["pages"];
+
+            currentPageNum++;
+            while (currentPageNum < pageCount)
+            {
+                string urlForPage = $"https://api.hh.ru/vacancies?text=c%23&area=72&only_with_salary=true&per_page=10&page={currentPageNum}";
+                var responseForPage = client.GetAsync(urlForPage).Result;
+                var responseForPageBody = responseForPage.Content.ReadAsStringAsync().Result;
+                JObject jsonForPage = JObject.Parse(responseForPageBody);
+                JArray vacanciesForPage = (JArray)jsonForPage["items"];
+
+                foreach (var vacancy in vacanciesForPage)
+                {
+                    vacancies.Add(vacancy);
+                }
+
+                currentPageNum++;
+            }
+
+            return vacancies;
         }
 
         private static float GetSalaryForVacancy(JToken vacancySalaryJson)
